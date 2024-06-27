2ad98d5d9fe5b5aa91b338512e3e38247cf3e1e0
Juan Manuel <juanmanuelcabrera.r@gmail.com>
2024-06-27 19:43:41 +0100

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--skip-torch-cuda-test --precision full --no-half --medvram
+set COMMANDLINE_ARGS=--skip-torch-cuda-test --precision full --no-half --medvram --use-cpu all --enable-insecure-extension-access
 
 call webui.bat
